5794983fa79f85d6938b6f65e46ab4b8e1e8c590
songjy <songjy@ethical.cn>
2017-07-17 13:06:42 +0800

    UPDATE: migrations add version for update

--- migrations.py
+++ migrations.py
@@ -2,58 +2,94 @@
 # -*- coding: utf-8 -*-
 
 import logging
+import arrow
 from playhouse.migrate import *
 from peewee import ProgrammingError
+
 from config import DB_CONFIG
-from models import *
+
+
+db = PostgresqlDatabase(**DB_CONFIG)
+migrator = PostgresqlMigrator(db)
 
 logger = logging.getLogger('sanic')
 
+class MigrationRecord(Model):
+    id = PrimaryKeyField()
+    table = CharField()
+    version = CharField()
+    author = CharField()
+    create_time = DateTimeField(verbose_name='创建时间',
+                                default=arrow.utcnow().datetime)
+
+    class Meta:
+        db_table = 'migration_record'
+        database = db
+
+def info(version=None, author=None, datetime=None):
+    def decorator(fn):
+        @functools.wraps(fn)
+        def _decorator(*args, **kwargs):
+            if not version: return None
+            table = args[0]._model._meta.db_table
+            mr = MigrationRecord.select().where(
+                MigrationRecord.version == version,
+                MigrationRecord.table == table
+            )
+            if mr: return None
+            MigrationRecord.create(
+                table=table, version=version, author=author if author else '',
+                datetime=datetime if datetime else arrow.utcnow().datetime)
+            fn(*args, **kwargs)
+        _decorator.version = version
+        _decorator.author = author
+        _decorator.datetime = datetime
+        return _decorator
+    decorator.version = version
+    decorator.author = author
+    decorator.datetime = datetime
+    return decorator
+
 class MigrationModel(object):
-    _db = None
-    _migrator = None
+    _db = db
+    _migrator = migrator
 
     def __init__(self):
+        self._mr = MigrationRecord()
+        self._db.create_table(self._mr, safe=True)
         if self._model:
-            dis = getattr(self._model._meta, 'distribute', None)
-            if dis:
-                qc = self._db.compiler()
-                qt = list(qc.create_table(self._model, safe=True))
-                qt[0] += dis
-                self._db.execute_sql(*tuple(qt))
-            else:
-                self._db.create_table(self._model, safe=True)
+            self._db.create_table(self._model, safe=True)
             self._name = self._model._meta.db_table
 
     def add_column(self, col, field=None):
         print('Migrating==> [%s] add_column: %s' % (self._name, col))
         field = getattr(self._model, col) if not field else field
-        return self.migrator.add_column(self._name, col, field)
+        return self._migrator.add_column(self._name, col, field)
 
     def rename_column(self, old, new):
         print('Migrating==> [%s] rename_column: (%s)-->(%s)' % (self._name, old, new))
-        return self.migrator.rename_column(self._name, old, new)
+        return self._migrator.rename_column(self._name, old, new)
 
     def drop_column(self, col):
         print('Migrating==> [%s] drop_column: %s' % (self._name, col))
-        return self.migrator.drop_column(self._name, col)
+        return self._migrator.drop_column(self._name, col)
 
     def drop_not_null(self, col):
         print('Migrating==> [%s] drop_not_null: %s' % (self._name, col))
-        return self.migrator.drop_not_null(self._name, col)
+        return self._migrator.drop_not_null(self._name, col)
 
     def add_not_null(self, col):
         print('Migrating==> [%s] add_not_null: %s' % (self._name, col))
-        return self.migrator.add_not_null(self._name, col)
+        return self._migrator.add_not_null(self._name, col)
 
     def rename_table(self, name):
         print('Migrating==> [%s] rename_table: %s' % (self._name, name))
-        return self.migrator.rename_table(self._name, name)
+        return self._migrator.rename_table(self._name, name)
 
     def add_index(self, cols, unique=False):
         print('Migrating==> [%s] add_index: %s' % (self._name, cols))
-        return self.migrator.add_index(self._name, cols, unique)
+        return self._migrator.add_index(self._name, cols, unique)
 
     def drop_index(self, col):
         print('Migrating==> [%s] drop_index: %s' % (self._name, col))
-        return self.migrator.drop_index(self._name, col)
+        return self._migrator.drop_index(self._name, col)
--- server.py
+++ server.py
@@ -16,7 +16,7 @@ from basictracer import BasicTracer
 from sanic import Sanic
 from sanic.handlers import ErrorHandler
 from sanic.response import json, text, HTTPResponse
-from sanic.exceptions import RequestTimeout
+from sanic.exceptions import RequestTimeout, NotFound
 from aiohttp import ClientSession
 
 from config import DB_CONFIG
@@ -35,6 +35,7 @@ with open('ethicall_common/logging.yml') as f:
 logger = logging.getLogger('sanic')
 # make app
 app = Sanic(__name__)
+app.config.ZIPKIN_SERVER = ""
 
 app.blueprint(openapi_blueprint)
 #app.blueprint(swagger_blueprint)
@@ -106,7 +107,7 @@ async def cros(request):
 
 @app.middleware('response')
 async def cors_res(request, response):
-    span = request['span']
+    span = request['span'] if 'span' in request else None
     if response is None:
         return response
     result = {'status': True}
@@ -119,16 +120,20 @@ async def cors_res(request, response):
         else:
             result.update({'data': response})
         response = json(result)
-        span.set_tag('http.status_code', "200")
+        if span: span.set_tag('http.status_code', "200")
     response.headers["Access-Control-Allow-Origin"] = "*"
     response.headers["Access-Control-Allow-Headers"] = "Content-Type"
     response.headers["Access-Control-Allow-Methods"] = "POST, PUT, DELETE"
-    await request.app.reporter.finish(request.app.name, span)
+    if span: await request.app.reporter.finish(request.app.name, span)
     return response
 
 @app.exception(RequestTimeout)
 def timeout(request, exception):
     return json({'message': 'Request Timeout'}, 408)
+
+@app.exception(NotFound)
+def notfound(request, exception):
+    return json({'message': 'Requested URL {} not found'.format(request.url)}, 404)
 
 def create_span(span_id, parent_span_id, trace_id, span_name,
                 start_time, duration, annotations,
