e556f0131c92ffb9567a7ee909b9da2763309610
songjy <songjy@ethicall.cn>
2017-09-13 10:13:30 +0800

    FIX: add user model and api

--- examples/models.py
+++ examples/models.py
@@ -3,12 +3,27 @@ from peewee import *
 from playhouse.postgres_ext import *
 from sanic_ms import doc
 
+class ProvinceApi:
+    id = int
+    name = doc.String('name')
+
+class CityApi:
+    id = int
+    name = doc.String("name")
+
+class RoleApi:
+    id = int
+    name = doc.String('name')
+
 class Users(Model):
     id = PrimaryKeyField()
-    create_time = DateTimeField(verbose_name='创建时间', default=datetime.datetime.utcnow)
-    name = CharField(max_length=128, verbose_name='名称')
-    age = IntegerField(null=False, verbose_name='年龄')
-    sex = CharField(max_length=32, verbose_name='性别')
+    create_time = DateTimeField(verbose_name='create time',
+                                default=datetime.datetime.utcnow)
+    name = CharField(max_length=128, verbose_name="user's name")
+    age = IntegerField(null=False, verbose_name="user's age")
+    sex = CharField(max_length=32, verbose_name="user's sex")
+    city_id = IntegerField(verbose_name='city for user', help_text=CityApi)
+    role_id = IntegerField(verbose_name='role for user', help_text=RoleApi)
 
     class Meta:
         db_table = 'users'
--- examples/views.py
+++ examples/views.py
@@ -14,15 +14,27 @@ logger = logging.getLogger('sanic')
 
 user_bp = Blueprint('user', url_prefix='users')
 
+@logger()
+async def get_city_by_id(request, id):
+    cli = request.app.client.cli(request)
+    async with cli.get('/cities/{}'.format(id)) as res:
+        return await res.json()
+
+@logger()
+async def get_role_by_id(request, id):
+    cli = request.app.client.cli(request)
+    async with cli.get('/roles/{}'.format(id)) as res:
+        return await res.json()
 
 async def async_request(datas):
+    # async handler request
     results = await asyncio.gather(*[data[2] for data in datas])
     for index, obj in enumerate(results):
         data = datas[index]
         data[0][data[1]] = results[index]
 
 @user_bp.get('/')
-@doc.summary("获取用户列表")
+@doc.summary("get user list")
 @doc.produces([Users])
 async def get_users_list(request):
     async with request.app.db.acquire(request) as cur:
@@ -30,13 +42,14 @@ async def get_users_list(request):
         return records
 
 @user_bp.get('/<id:int>')
-@doc.summary("获取用户信息")
-@doc.produces([Users])
+@doc.summary("get user info")
+@doc.produces(Users)
 async def get_users_list(request, id):
     async with request.app.db.acquire(request) as cur:
-        record = await cur.fetch(
+        records = await cur.fetch(
             """ SELECT * FROM users WHERE id = $1 """, id)
         datas = [
-            [record, '', get_articles_by_id(request, record['articles'])]
+            [records, 'city_id', get_city_by_id(request, records['city_id'])]
+            [records, 'role_id', get_role_by_id(request, records['role_id'])]
         ]
-        return record
+        return records
